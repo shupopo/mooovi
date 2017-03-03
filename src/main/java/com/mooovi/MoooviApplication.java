d71697b8f3279fdab2c95ea7c8d6359d0c8663eb
Fumiya <Fumiya@Fumiya-MacBook-Pro.local>
2017-03-03 16:09:48 +0900

    First commit

---
+++ src/main/java/com/mooovi/MoooviApplication.java
@@ -0,0 +1,12 @@
+package com.mooovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoooviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoooviApplication.class, args);
+	}
+}
