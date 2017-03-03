2414e7f6e39b49bfe5224c6d1f6e8af89e429d38
Fumiya <Fumiya@Fumiya-MacBook-Pro.local>
2017-03-03 18:33:20 +0900

    Untabify

--- src/main/java/com/mooovi/MoooviApplication.java
+++ src/main/java/com/mooovi/MoooviApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MoooviApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MoooviApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MoooviApplication.class, args);
+    }
 }
