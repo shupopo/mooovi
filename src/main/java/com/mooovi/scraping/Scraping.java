6a30d576e3bd632f986c96911c372997d2ac98c1
Fumiya <Fumiya@Fumiya-MacBook-Pro.local>
2017-03-04 00:47:42 +0900

    Add scraping

---
+++ src/main/java/com/mooovi/scraping/Scraping.java
@@ -0,0 +1,32 @@
+package com.mooovi.scraping;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.mooovi.business.service.ProductService;
+
+@Component
+public class Scraping {
+
+    @Autowired
+    private ProductService productService;
+
+    private static final String SITE_URL = "http://review-movie.herokuapp.com/";
+
+    private List<String> links = new ArrayList<String>();
+
+    public void execute() throws IOException {
+        System.out.println("execute scraping!");
+    }
+
+    private void collectPageLink() throws IOException {
+    }
+
+    private void saveProduct(String link) throws IOException {
+    }
+
+}
